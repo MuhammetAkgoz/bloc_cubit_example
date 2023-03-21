2cd9301e8a14a2eddfd5a6b09416b6c76258d81f
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-03-22 00:55:46 +0300

    cubit service example

--- lib/example/example_view.dart
+++ lib/example/example_view.dart
@@ -2,23 +2,34 @@ import 'package:bloc_cubit_example/example/example_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ExampleView extends StatelessWidget {
+class ExampleView extends StatefulWidget {
   const ExampleView({Key? key}) : super(key: key);
+
+  @override
+  State<ExampleView> createState() => _ExampleViewState();
+}
+
+class _ExampleViewState extends State<ExampleView> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<ExampleViewModel>().get();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Example App')),
       body: BlocBuilder<ExampleViewModel, ExampleState>(
-        builder: (context, state) => Center(child: Text(state.number.toString())),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(child: const Icon(Icons.add), onPressed: () => context.read<ExampleViewModel>().increase()),
-          const SizedBox(height: 20),
-          FloatingActionButton(child: const Icon(Icons.remove), onPressed: () => context.read<ExampleViewModel>().decrease()),
-        ],
+        builder: (context, state) => state.serviceState != ServiceState.success
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: state.model.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(state.model[index].title ?? ''),
+                  subtitle: Text(state.model[index].body ?? ''),
+                ),
+              ),
       ),
     );
   }
--- lib/example/example_view_model.dart
+++ lib/example/example_view_model.dart
@@ -1,33 +1,56 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:bloc_cubit_example/example/example_response.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
 class ExampleViewModel extends Cubit<ExampleState> {
-  ExampleViewModel() : super(ExampleState(number: 5));
+  ExampleViewModel() : super(const ExampleState(number: 5, model: []));
 
-  void increase() {
-    int result = state.number + 1;
-    emit(state.copyWith(number: result));
-  }
+  final String baseUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-  void decrease() {
-    int result = state.number - 1;
-    emit(state.copyWith(number: result));
+  Future<void> get() async {
+    final response = await http.get(Uri.parse(baseUrl));
+
+    if (response.statusCode == HttpStatus.ok) {
+      List<dynamic> body = jsonDecode(response.body);
+
+      return emit(state.copyWith(
+        model: body.map((dynamic item) => ExampleResponseModel.fromJson(item)).toList(),
+        serviceState: ServiceState.success,
+      ));
+    }
+    throw Exception('fetch error');
   }
 }
 
+enum ServiceState { loading, success, error }
+
 class ExampleState extends Equatable {
   final int number;
+  final List<ExampleResponseModel> model;
+  final ServiceState serviceState;
 
-  ExampleState({required this.number});
+  const ExampleState({
+    required this.number,
+    required this.model,
+    this.serviceState = ServiceState.loading,
+  });
 
   @override
-  List<Object?> get props => [number];
+  List<Object?> get props => [number, model, serviceState];
 
   ExampleState copyWith({
     int? number,
+    List<ExampleResponseModel>? model,
+    ServiceState? serviceState,
   }) {
     return ExampleState(
       number: number ?? this.number,
+      model: model ?? this.model,
+      serviceState: serviceState ?? this.serviceState,
     );
   }
 }
