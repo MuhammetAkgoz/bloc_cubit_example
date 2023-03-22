5f594bb0b95e01823b9a18e052f28ea6508dc9be
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-03-22 13:29:09 +0300

    base_view_model structure

---
+++ lib/core/base/base_stateless_view.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+/// [BaseStatelessView] a base of all [StatelessWidget] classes
+abstract class BaseStatelessView extends StatelessWidget {
+  const BaseStatelessView({super.key});
+}
